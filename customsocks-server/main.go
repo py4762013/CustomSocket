b38c92dea2f1eaad6663e3b528e60f55b9e27ddd
PengYe <pengye@socialecommerce.com>
2019-12-04 19:40:18 +0800

    custom server

---
+++ customsocks-server/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+
+	"github.com/py4762013/CustomSocket"
+	"github.com/py4762013/CustomSocket/cmd"
+	"github.com/phayes/freeport"
+)
